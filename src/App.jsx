6154d1e552ae03a6eea04afee458119dce3c7e4e
pankaj yadav <pankajyadav3829@gmail.com>
2026-02-20 14:10:22 +0530

    starting project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App(){
+  return (
+    <div className="container mx-auto p-4">
+      <h1>NetflixGPT</h1>
+      <p>Ask NetflixGPT to recommend you a movie or TV show based on your preferences.</p>
+      <p>Example: "Recommend me a comedy movie with a strong female
+lead."</p>
+    </div>
+  );
+}
